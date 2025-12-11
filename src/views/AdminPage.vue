2ec3994df3758cd8791ba977fbcbfb53453d1838
LazerThings <ibananiza@icloud.com>
2025-12-11 14:00:25 -0600

    Add onMounted import to AdminPage.vue

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useAuth } from '../composables/useAuth'
 import AppLayout from '../components/layout/AppLayout.vue'
 import type { User, UserRole } from '../types'
